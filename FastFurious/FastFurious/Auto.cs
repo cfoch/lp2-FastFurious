529ce43e8d671fdde185ec4abdf01278d765507a
Fabian Orccon <fabian.orccon@pucp.pe>
2017-04-05 10:04:38 -0500

    src: Agregar clases para Auto y Camioneta

---
+++ FastFurious/FastFurious/Auto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFurious
+{
+    public class Auto : AutoCamioneta
+    {
+        public Auto(int codigo, string marca, string modelo, string color, int año, float precioAdquisicion, float precioVenta, EstadoTipo estado)
+            : base (codigo, marca, modelo, color, año, precioAdquisicion, precioVenta, estado)
+        {
+
+        }
+    }
+}
---
+++ FastFurious/FastFurious/Camioneta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFurious
+{
+    class Camioneta : AutoCamioneta
+    {
+        public Camioneta(int codigo, string marca, string modelo, string color, int año, float precioAdquisicion, float precioVenta, EstadoTipo estado)
+            : base (codigo, marca, modelo, color, año, precioAdquisicion, precioVenta, estado)
+        {
+
+        }
+    }
+}
